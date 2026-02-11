518b79120c0bec038c1432781b61f5eb8c1cf631
Blaine <blainenewvillage@gmail.com>
2026-02-11 05:18:33 +0400

    recommit

--- src/app/articles/[slug]/page.tsx
+++ src/app/articles/[slug]/page.tsx
@@ -1,14 +1,9 @@
 import path from 'path';
 import fs from 'fs/promises';
+import { Article } from '@/app/lib/definitions';
 
 type Props = {params: {slug: string}};
-type Article = {
-    title: string,
-    slug: string,
-    content: string,
-    author?: string,
-    image?: string,
-};
+
 
 export default async function ArticlePage({params} : Props) {
     const filePath = path.join(process.cwd(), 'public', 'articles.json')
